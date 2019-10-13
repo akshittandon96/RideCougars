98b1866b97456830e71cef63ba518e210db120e8
Team3 <team3@ubicomps-MacBook-Air.local>
2019-10-13 13:53:31 -0500

    test

---
+++ RideCougars/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RideCougars
+//
+//  Created by Team3 on 10/13/19.
+//  Copyright © 2019 Team3. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RideCougars/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RideCougars
+//
+//  Created by Team3 on 10/13/19.
+//  Copyright © 2019 Team3. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
